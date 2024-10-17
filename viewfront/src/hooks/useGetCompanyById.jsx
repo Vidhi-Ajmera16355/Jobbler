e6ffff9762360dd3dcd5de098a341c492b9d87d1
Vidhi <vidhi2005ajmera@gmail.com>
2024-10-17 15:42:33 +0530

    commit3

--- viewfront/src/hooks/useGetCompanyById.jsx
+++ viewfront/src/hooks/useGetCompanyById.jsx
@@ -13,7 +13,7 @@ const useGetCompanyById = (companyId) => {
                 const res = await axios.get(`${COMPANY_API_END_POINT}/get/${companyId}`, { withCredentials: true });
                 console.log(res.data.company)
                 if (res.data.success) {
-                    dispatch(setSingleCompany(res.data.jobs));
+                    dispatch(setSingleCompany(res.data.company));
                 }
             }
             catch (error) {
